d0dc5a040a150f4fcd2fd7b26546692366c75267
mjtolmo <bootcamp.javascript.fullstack@gmail.com>
2022-11-15 17:34:43 -0300

    Unimarc leyendo ok

--- super_unimarc/unimarcArrozCategoria.js
+++ super_unimarc/unimarcArrozCategoria.js
@@ -23,22 +23,52 @@ const scanUnimarc = async () => {
 
         let contentProduct = { selector: "div .Shelf_shelf__WM77V" };
         let simboloOferta = { class: "JMKOB" };
-        let imagenProducto = { class: "prod__figure__img" };
-        let precioBase = { class: "base__price" };
-        let marcaProducto = { class: "Shelf_brandText__sGfsS" }; //
-        let nombreProducto = { class: "Shelf_nameProduct__CXI5M" }; //
-        let cantProducto = { class: "Text_text--left__1v2Xw" }; //
+        let imagenProducto = { selector: ".picture_picture__QMdfM > img" };
+        let prBase = {
+          class: "",
+          selector: "div .Shelf_shelf__WM77V > div > div > div > div > div",
+          item: {
+            0: { precioBase: 0, precioPorUnidad: 2 },
+            1: { precioBaseOferta: 0, precioPorUnidadOferta: 2 },
+          },
+        }; //
+        let marcaProducto = {
+          class: "Shelf_brandText__sGfsS",
+          selector: "div .Shelf_shelf__WM77V > div > div > a > div > p",
+          item: 0,
+        }; //
+        let nombreComletoProducto = {
+          class: "Shelf_nameProduct__CXI5M",
+          selector: "div .Shelf_shelf__WM77V > div > div > a > div > p",
+          item: 1,
+          selectorB: ["div .Shelf_shelf__WM77V > div > div > a", "title"],
+        };
+        let nombreProducto = {
+          class: "div .Shelf_shelf__WM77V > div > div > a",
+        }; //title
+        let cantProducto = {
+          class: "Text_text--left__1v2Xw",
+          selector: "div .Shelf_shelf__WM77V > div > div > a > div > p",
+          item: 2,
+        }; //
         let pack = { class: "prod__n-per-price__text" };
 
-        let cont = document.querySelectorAll(contentProduct.selector);
-        console.log("cont :>> ", cont);
-        productos.push(cont[0]);
+        let imgElementos = document.querySelectorAll(imagenProducto.selector);
+        imgElementos.forEach((img) => {
+          productos.push(img.src != null ? img.src : null);
+        });
+        let preciosElementos = document.querySelectorAll(prBase.selector);
+        preciosElementos.forEach((precios) => {
+          precios.children[0].children[0].textContent;
+        });
+        ///CONTINUAR
+
         console.log("productos :>> ", productos);
 
         return productos;
       }, footerScroll);
       await page.waitForTimeout(1256);
-      console.log("recuperados :>> ", recuperados);//no sale de evaluate
+      console.log("recuperados :>> ", recuperados); //no sale de evaluate
       //browser.close();
     } catch (error) {
       console.log("error pronames :>> ", error);
